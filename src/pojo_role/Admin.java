e682842e7129aa2d85a33d01a11eee780247a14d
CalviStier <38694925+putrifus@users.noreply.github.com>
2018-06-18 08:03:39 +0200

    double rien

--- src/pojo_role/Admin.java
+++ src/pojo_role/Admin.java
@@ -4,11 +4,12 @@ public class Admin extends Role {
 	public Admin() {
 		setModifTheme(true);
 		setAddUser(true);
-		
+
 		setAddArticle(true);
 		setCreateArticle(true);
 		setEditArticle(true);
-		
+
 		setModo(true);
+
 	}
 }
